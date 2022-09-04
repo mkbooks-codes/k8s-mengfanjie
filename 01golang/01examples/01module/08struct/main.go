3e2a6b8e9c747da07fedfe4fb27dc6d21b081ce4
cjx <1067446576@qq.com>
2022-09-04 21:15:17 +0800

    add: struct

---
+++ 01golang/01examples/01module/08struct/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+)
+
+type MyType struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	mt := MyType{Name: "test"}
+	myType := reflect.TypeOf(mt)
+	println(myType)
+	name := myType.Field(0)
+	tag := name.Tag.Get("json")
+	println(tag)
+	tb := TypeB{P2: "p2", TypeA: TypeA{P1: "p1"}}
+	//可以直接访问 TypeA.P1
+	println(tb.P1)
+}
+
+type TypeA struct {
+	P1 string
+}
+
+type TypeB struct {
+	P2 string
+	TypeA
+}
