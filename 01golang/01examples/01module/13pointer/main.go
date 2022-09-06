7fc2d7c4e6fa5fc87acfd5f2a919b60decfb89e4
cjx <1067446576@qq.com>
2022-09-06 21:52:21 +0800

    add: 语言特性:callback, pointer

---
+++ 01golang/01examples/01module/13pointer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "a string value"
+	pointer := &str
+	anotherString := *&str
+	fmt.Println(str)
+	fmt.Println(pointer)
+	fmt.Println(anotherString)
+	str = "changed string"
+	fmt.Println(str)
+	fmt.Println(pointer)
+	fmt.Println(anotherString)
+
+	fmt.Println()
+	para := ParameterStruct{Name: "aaa"}
+	fmt.Println(para)
+	changeParameter(&para, "bbb")
+	fmt.Println(para)
+	cannotChangeParameter(para, "ccc")
+	fmt.Println(para)
+}
+
+type ParameterStruct struct {
+	Name string
+}
+
+func changeParameter(para *ParameterStruct, value string) {
+	para.Name = value
+}
+
+func cannotChangeParameter(para ParameterStruct, value string) {
+	para.Name = value
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/mkbooks-codes/k8s-mengfanjie
 
 go 1.17
 
-require github.com/magiconair/properties v1.8.6
+require (
+	github.com/magiconair/properties v1.8.6
+	github.com/stretchr/testify v1.8.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
